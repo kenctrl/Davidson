05d6d0acca2d522f737c6da066c4f1e9b8368b1d
kenctrl <54418801+Kenny5056@users.noreply.github.com>
2020-07-01 02:48:50 -0400

    7/1 midnight edits

---
+++ hamiltonianscale.m
@@ -0,0 +1,14 @@
+Hnew = zeros(size(H));
+for ii = 1:N
+    for jj = 1:N
+        if ii == jj
+            Hnew(ii,jj) = min(ii,N - ii) * H(ii,jj);
+        elseif ii == jj + 1
+            Hnew(ii,jj) = min(ii - 1,N + 1 - ii) * H(ii,jj);
+        elseif ii == jj - 1
+            Hnew(ii,jj) = min(ii,N - ii) * H(ii,jj);
+        else
+            Hnew(ii,jj) = H(ii,jj);
+        end
+    end
+end
