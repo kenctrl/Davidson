af1d44cda0d97352e375553537f578020be59581
kenctrl <54418801+Kenny5056@users.noreply.github.com>
2020-07-15 02:37:29 -0400

    7/15 midnight edits

--- PCN.m
+++ PCN.m
@@ -1,4 +1,4 @@
-function [v_PC] = PCN(N,dt,a,m,subdivisions,Hobj)
+function [v_PC,v_exact] = PCN(N,dt,a,m,subdivisions,Hobj)
 [r,c] = size(Hobj);
 HPC = HPCconstrN(N,a,m,Hobj);
 v = wavefunctionPCN(r,N);
@@ -15,4 +15,16 @@ for kk = 1:r
     v_PC(kk,1) = psi1((kk-1)*N+1,1) + psi1((kk-1)*N+2,1);
 end
 index = find(abs(v_PC) == max(abs(v_PC)));
-v_PC = v_PC/v_PC(index);
+v_PC = v_PC/v_PC(index(1,1));
+
+[eigenvectors, diagonalized_matrix] = eig(Hobj);
+% eigenvalues = diag(diagonalized_matrix);
+% [sorted_list, ordering] = eigenvalues;
+% eigenvectors(:,ordering(1));
+v_exact = eigenvectors(:,1);
+index = find(abs(v_exact) == max(abs(v_exact)));
+v_exact = v_exact/v_exact(index(1,1));
+
+% v_exact = eig(Hobj);
+% index = find(abs(v_exact) == max(abs(v_exact)));
+% v_exact = v_exact/v_exact(index(1,1));
